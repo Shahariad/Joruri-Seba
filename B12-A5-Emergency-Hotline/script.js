c65590ef1e6ead4205734d2789c392848a6879cf
Shahariad <shahariadakash2@gmail.com>
2025-08-28 14:44:02 +0600

    first commit

---
+++ B12-A5-Emergency-Hotline/script.js
@@ -0,0 +1,12 @@
+console.log("Java connected")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // 👈 Update paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
